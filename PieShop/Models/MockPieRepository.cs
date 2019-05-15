7f67075dd5b8fa0aa891811d3f50250e1e63a3b7
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-05-15 18:21:11 +0200

    added model

---
+++ PieShop/Models/MockPieRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.Models
+{
+    public class MockPieRepository : IPieRepository
+    {
+        private List<Pie> _pies;
+
+        public MockPieRepository()
+        {
+            if(_pies == null)
+            {
+                InitializePies();
+            }
+        }
+
+        private void InitializePies()
+        {
+            _pies = new List<Pie>
+            {
+                new Pie {Id = 1, Name = "Pie", Price = 12.96M, ShortDescription ="Super Pie", LongDescription="Super Super Pie" },
+                new Pie {Id = 2, Name = "SPie", Price = 12.96M, ShortDescription ="Super Pie", LongDescription="Super Super Pie" },
+                new Pie {Id = 3, Name = "APie", Price = 12.96M, ShortDescription ="Super Pie", LongDescription="Super Super Pie" },
+                new Pie {Id = 4, Name = "CPie", Price = 12.96M, ShortDescription ="Super Pie", LongDescription="Super Super Pie" },
+                new Pie {Id = 5, Name = "XPie", Price = 12.96M, ShortDescription ="Super Pie", LongDescription="Super Super Pie" }
+            };
+        }
+
+        public IEnumerable<Pie> GetAllPies()
+        {
+            return _pies;
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return _pies.FirstOrDefault(p => p.Id == pieId);
+        }
+    }
+}
