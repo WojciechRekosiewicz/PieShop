bea316de43b9c8f1730b7e071ed4baa2f1897899
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-05-15 18:51:52 +0200

    start adding controller

---
+++ PieShop/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+
+namespace PieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            pieRepository = _pieRepository;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
