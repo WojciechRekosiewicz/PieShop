1f830efb1de19cb9efb72bdb5a8fb3cdadd945fd
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-05-16 20:08:25 +0200

    added local database and performed migration

---
+++ PieShop/Models/PieRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly AppDbContext _appDBContext;
+
+        public PieRepository(AppDbContext appDBContext)
+        {
+            _appDBContext = appDBContext;
+        }
+
+
+        public IEnumerable<Pie> GetAllPies()
+        {
+            return _appDBContext.Pies;
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return _appDBContext.Pies.FirstOrDefault(p => p.Id == pieId);
+        }
+    }
+}
