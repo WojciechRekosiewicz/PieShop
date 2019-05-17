e36f09297de940471e6f93199e8e11593cb9b850
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-05-17 19:52:50 +0200

    added login/register to menu

--- PieShop/Areas/Identity/IdentityHostingStartup.cs
+++ PieShop/Areas/Identity/IdentityHostingStartup.cs
@@ -15,6 +15,8 @@ namespace PieShop.Areas.Identity
         public void Configure(IWebHostBuilder builder)
         {
             builder.ConfigureServices((context, services) => {
+                services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<AppDbContext>();
             });
         }
     }
